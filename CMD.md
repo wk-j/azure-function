13714787f4e054b95449e005db197fef50055220
Somnuk Wongkhan <somnuk.wk@gmail.com>
2018-01-09 16:14:43 +0700

    Http trigger

--- CMD.md
+++ CMD.md
@@ -7,7 +7,9 @@ func init .
 func new
 
 dotnet build src/WkFunctions
-dotnet publish src/WkFunctions --output ../../publish
+dotnet publish src/WkFunctions --output ../../publish/Timer
 
 func start --script-root publish
+
+dotnet publish src/WkHttpTrigger --output ../../publish/Http
 ```
---
+++ src/WkHttpTrigger/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+namespace WkHttpTrigger
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Http
+open Microsoft.Azure.WebJobs.Host
+
+module Program = 
+    let run(req: HttpRequest, log: TraceWriter) =
+        log.Info("F# HTTP trigger function processed a request.")
+        ContentResult(Content = "HO HO HO Merry Christmas", ContentType = "text/html")
